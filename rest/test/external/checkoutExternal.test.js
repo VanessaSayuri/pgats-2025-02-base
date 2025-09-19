a847ca310d12a4eb43616a096c9b92ccb399ce10
VanessaSayuri <sayurivanessa@gmail.com>
2025-09-19 20:33:31 -0300

    adicionando testes de API via http

---
+++ rest/test/external/checkoutExternal.test.js
@@ -0,0 +1,98 @@
+//Bibliotecas
+const request = require('supertest')
+const { expect } = require ('chai')
+
+
+//Testes
+
+describe('Checkout External', () => {
+    describe('POST /api/checkout', () =>{
+
+        beforeEach(async () =>{
+            const respostaLogin = await request('http://localhost:3000')
+                .post('/api/users/login')
+                .send({
+                    email: "alice@email.com",
+                    password: "123456"
+                    });
+        token = respostaLogin.body.token;
+        })
+
+        it('Quando informo id de produto valido e pagamento com boleto status 200', async () => {
+
+            const resposta = await request('http://localhost:3000')
+                .post('/api/checkout')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    items: [
+                        {
+                        productId: 2,
+                        quantity: 2
+                        }
+                    ],
+                    freight: 0,
+                    paymentMethod: "boleto"
+                });
+
+            expect(resposta.status).to.equal(200);
+            
+            respostaEsperada = require('../fixture/respostas/quandoInformoValoresValidosBoletoTenhoSucessoCom200.json')
+            
+            expect(resposta.body).to.deep.equal(respostaEsperada)
+        })
+                it('Quando informo id de produto valido e pagamento com cartão de crédito status 200', async () => {
+
+            const resposta = await request('http://localhost:3000')
+                .post('/api/checkout')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    items: [
+                        {
+                        productId: 2,
+                        quantity: 2
+                        }
+                    ],
+                    freight: 0,
+                    paymentMethod: "credit_card",
+                    cardData: {
+                        number: "4111111111111111",
+                        name: "Alice Souza",
+                        expiry: "12/30",
+                        cvv: "123"
+                    }
+                });
+
+            expect(resposta.status).to.equal(200);
+            
+            respostaEsperada = require('../fixture/respostas/quandoInformoValoresValidosCartaoDeCreditoTenhoSucessoCom200.json')
+            
+            expect(resposta.body).to.deep.equal(respostaEsperada)
+        })
+        
+        it('Quando informo id de produto invalido recebo Erro 400', async () => {
+
+            const resposta = await request('http://localhost:3000')
+                .post('/api/checkout')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    items: [
+                        {
+                        productId: 7,
+                        quantity: 2
+                        }
+                    ],
+                    freight: 0,
+                    paymentMethod: "boleto",
+                    cardData: {
+                        number: "string",
+                        name: "string",
+                        expiry: "string",
+                        cvv: "string"
+                    }
+                });
+                
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Produto não encontrado')
+        })
+    })
+})
