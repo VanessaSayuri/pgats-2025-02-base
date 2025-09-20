215ccdbfd48bbb4c68f5d711a4dfe506f5545e93
VanessaSayuri <sayurivanessa@gmail.com>
2025-09-20 11:57:59 -0300

    adicionando testes de valicacao do controller

---
+++ rest/test/controller/checkoutController.test.js
@@ -0,0 +1,116 @@
+//Bibliotecas
+const request = require('supertest');
+const { expect } = require ('chai');
+const sinon = require('sinon');
+
+// Aplicação
+const app = require('../../app');
+
+//Mock
+const checkoutService = require('../../../src/services/checkoutService')
+
+//Testes
+
+describe('Checkout External', () => {
+    describe('POST /api/checkout', () =>{
+
+        beforeEach(async () =>{
+            const respostaLogin = await request(app)
+                .post('/api/users/login')
+                .send({
+                    email: "alice@email.com",
+                    password: "123456"
+                    });
+        token = respostaLogin.body.token;
+        })
+
+        it('Mock: Quando informo id de produto valido e pagamento com boleto status 200', async () => {
+
+            const checkoutServiceMock = sinon.stub(checkoutService, 'checkout');
+            respostaEsperada = require('../fixture/respostas/quandoInformoValoresValidosBoletoTenhoSucessoCom200.json')
+            checkoutServiceMock.returns(respostaEsperada)
+
+            const resposta = await request(app)
+                .post('/api/checkout')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    items: [
+                        {
+                        productId: 2,
+                        quantity: 2
+                        }
+                    ],
+                    freight: 0,
+                    paymentMethod: "boleto"
+                });
+
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body).to.deep.equal(respostaEsperada)
+        })
+        it('Mock: Quando informo id de produto valido e pagamento com cartão de crédito status 200', async () => {
+
+            const checkoutServiceMock = sinon.stub(checkoutService, 'checkout');
+            respostaEsperada = require('../fixture/respostas/quandoInformoValoresValidosCartaoDeCreditoTenhoSucessoCom200.json')
+            checkoutServiceMock.returns(respostaEsperada)
+
+
+            const resposta = await request(app)
+                .post('/api/checkout')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    items: [
+                        {
+                        productId: 2,
+                        quantity: 2
+                        }
+                    ],
+                    freight: 0,
+                    paymentMethod: "credit_card",
+                    cardData: {
+                        number: "4111111111111111",
+                        name: "Alice Souza",
+                        expiry: "12/30",
+                        cvv: "123"
+                    }
+                });
+            
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body).to.deep.equal(respostaEsperada)
+
+        })
+        
+        it('Mock: Quando informo id de produto invalido recebo Erro 400', async () => {
+
+            const checkoutServiceMock = sinon.stub(checkoutService, 'calculateTotal');
+            checkoutServiceMock.throws(new Error('Produto não encontrado'))
+
+            const resposta = await request(app)
+                .post('/api/checkout')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    items: [
+                        {
+                        productId: 7,
+                        quantity: 2
+                        }
+                    ],
+                    freight: 0,
+                    paymentMethod: "boleto",
+                    cardData: {
+                        number: "string",
+                        name: "string",
+                        expiry: "string",
+                        cvv: "string"
+                    }
+                });
+                
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Produto não encontrado')
+        });
+
+        //Resetar o mock
+        afterEach(() => {
+            sinon.restore();
+        })
+    });
+});
